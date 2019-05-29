4a938b624cb9e6c78ea474a42263534d3509ab57
mwintherDK <maha@aspit.dk>
2019-05-29 09:59:47 +0200

    Har lavet en ny awesome feature og skrevet en dum kommentari

--- js/script.js
+++ js/script.js
@@ -5,26 +5,40 @@
 let names = ["Arne", "Balder", "Calle", "Doriiiith", "Einar", "Franke"];
 
 if (names.length > 3) {
-    console.log(`Der er ${names.length} navne i dit array!`);
+  console.log(`Der er ${names.length} navne i dit array!`);
 } else {
-    console.log(`Det er under fire navne i dit array`);
+  console.log(`Det er under fire navne i dit array`);
 }
 // Asger
-console.log(names.length > 3 ? `Der er ${names.length} navne i dit array!` : `Det er under fire navne i dit array`);
+console.log(
+  names.length > 3
+    ? `Der er ${names.length} navne i dit array!`
+    : `Det er under fire navne i dit array`
+);
 //
 
 // Maren
-console.log((names.length > 3) ? `Der er ${names.length} navne i dit array!` : 'Det er under fire navne i dit array');
+console.log(
+  names.length > 3
+    ? `Der er ${names.length} navne i dit array!`
+    : "Det er under fire navne i dit array"
+);
 //
 
 // Birk
-let nameStringBirk = (names.length > 3) ? `Der er ${names.length} navne i dit array!` : `Det er under fire navne i dit array`;
+let nameStringBirk =
+  names.length > 3
+    ? `Der er ${names.length} navne i dit array!`
+    : `Det er under fire navne i dit array`;
 console.log(nameStringBirk);
 //
 
 // Nickolai
 console.log(
-    names.length > 3 ? `Der er ${names.length} navne i dit array` : `Det er under fire navne i dit array`);
+  names.length > 3
+    ? `Der er ${names.length} navne i dit array`
+    : `Det er under fire navne i dit array`
+);
 //
 
 // Nicklas
@@ -35,13 +49,13 @@ names.length > 3
 
 // 2:
 let jamie = {
-    nick: "Kingslayer",
-    name: "Jamie Lannister",
-    girlfriend: {
-        nick: "Protector of the Realm",
-        name: "Cersei Lannister"
-    },
-    alive: false
+  nick: "Kingslayer",
+  name: "Jamie Lannister",
+  girlfriend: {
+    nick: "Protector of the Realm",
+    name: "Cersei Lannister"
+  },
+  alive: false
 };
 
 // Asger
@@ -53,7 +67,7 @@ let jamie = {
 //
 
 // Birk
-let jamieAliveBirk = (jamie.alive) ? "Hes alive! Hurrah" : "Awww...Jamie is dead";
+let jamieAliveBirk = jamie.alive ? "Hes alive! Hurrah" : "Awww...Jamie is dead";
 console.log(jamieAliveBirk);
 //
 
@@ -68,9 +82,10 @@ jamie.alive
 //
 
 if (jamie.alive) {
-    console.log("Hes alive! Hurrah");
+  //Dum kommentar
+  console.log("Hes alive! Hurrah");
 } else {
-    console.log("Awww...Jamie is dead");
+  console.log("Awww...Jamie is dead");
 }
 
 // 3:
@@ -78,13 +93,13 @@ let clientWindowWidth = window.innerWidth;
 let container = document.querySelector(".container");
 
 if (clientWindowWidth > 1200) {
-    container.classList.add("hugeScreen");
+  container.classList.add("hugeScreen");
 } else {
-    container.classList.remove("hugeScreen");
+  container.classList.remove("hugeScreen");
 }
 
 // Asger
-
+let noget = "Her er sn løsning af et eller andet";
 //
 
 // Maren
@@ -92,11 +107,15 @@ if (clientWindowWidth > 1200) {
 //
 
 // Birk
-clientWindowWidth > 1200 ? container.classList.add("hugeScreen") : container.classList.remove("hugeScreen");
+clientWindowWidth > 1200
+  ? container.classList.add("hugeScreen")
+  : container.classList.remove("hugeScreen");
 //
 
 // Nickolai
-clientWindowWidth > 1200 ? container.classList.add("hugeScreen") : container.classList.remove("hugeScreen");
+clientWindowWidth > 1200
+  ? container.classList.add("hugeScreen")
+  : container.classList.remove("hugeScreen");
 //
 
 // Nicklas
